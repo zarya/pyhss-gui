ddd9b7c298e24ce084e1bf056004eae889965479
Rudy (zarya) <git@gigafreak.net>
2023-10-15 13:30:33 +0200

    Added basic tft generator

--- src/components/tft/add.tsx
+++ src/components/tft/add.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
 
 import {SelectField, SaveButtons, InputField} from '@components';
 import i18n from '@app/utils/i18n';
@@ -28,7 +29,14 @@ const TftAddItem = (props: {
   edit: boolean
 }) => {
   const { open, handleClose, data, edit } = props;
-  const [state, setState] = React.useState(data);
+  const [state, setState] = useState(data);
+  const [action, setAction] = useState('');
+  const [gen_direction, setDirection] = useState('');
+  const [protocol, setProtocol] = useState('');
+  const [source, setSource] = useState('');
+  const [destination, setDestination] = useState('');
+  const [selectedOption, setSelectedOption] = useState('');
+  const [filterResult, setFilterResult] = useState('');
 
   React.useEffect(() => {
     setState(data);
@@ -55,6 +63,20 @@ const TftAddItem = (props: {
 
   const handleLocalClose = () => {
     handleClose();
+  }
+
+  const generateFilter = () => {
+    const filterRule = `${action}${gen_direction ? ' ' + gen_direction : ''} ${protocol} from ${source} to ${destination}${selectedOption ? ' ' + selectedOption : ''}`;
+    handleChange('tft_string', filterRule); 
+  };
+
+  const clearGenerator = () => {
+    setSelectedOption('');
+    setDestination('');
+    setSource('');
+    setDirection('');
+    setProtocol('');
+    setAction('');
   }
 
   return (
@@ -106,8 +128,96 @@ const TftAddItem = (props: {
               </SelectField>
             </Grid>
           </Grid>
-         </Box>
+        </Box>
         <SaveButtons onClickClose={handleLocalClose} onClickSave={handleSave} />
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+        >
+          <Grid container rowSpacing={1} spacing={1}>
+            <Grid item xs={10}><h3>TFT Generator</h3></Grid>
+            <Grid item xs={2}>
+              <Button onClick={clearGenerator}>Clear</Button>
+              <Button onClick={generateFilter} variant="contained">Generate <i class="fas fa-arrow-up"></i></Button>
+            </Grid>
+            <Grid item xs={1}>
+              <SelectField
+                required
+                value={action}
+                onChange={(name, value) => setAction(value)}
+                id="action"
+                label="Action"
+                helper=""
+              >
+                <MenuItem value="permit">permit</MenuItem>
+                <MenuItem value="deny">deny</MenuItem>
+              </SelectField> 
+            </Grid>
+            <Grid item xs={1}>
+              <SelectField
+                value={gen_direction}
+                onChange={(name, value) => setDirection(value)}
+                id="gen_direction"
+                label="Direction"
+                helper=""
+              >
+                <MenuItem value="in">in</MenuItem>
+                <MenuItem value="out">out</MenuItem>
+              </SelectField> 
+            </Grid>
+            <Grid item xs={1}>
+              <SelectField
+                required
+                value={protocol}
+                onChange={(name, value) => setProtocol(value)}
+                id="protocol"
+                label="Protocol"
+                helper=""
+              >
+                <MenuItem value="ip">ip (any)</MenuItem>
+                <MenuItem value="tcp">tcp</MenuItem>
+                <MenuItem value="udp">udp</MenuItem>
+              </SelectField> 
+            </Grid>
+            <Grid item xs={3}>
+              <InputField
+                required
+                label="Source"
+                id="source"
+                onChange={(name, value) => setSource(value)}
+                value={source}
+              >Source (prefix or any)</InputField>
+            </Grid>
+            <Grid item xs={3}>
+              <InputField
+                required
+                label="Destination"
+                id="destination"
+                onChange={(name, value) => setDestination(value)}
+                value={destination}
+              >Destination (prefix or any)</InputField>
+            </Grid>
+            <Grid item xs={2}>
+              <SelectField
+                value={selectedOption}
+                onChange={(name, value) => setSelectedOption(value)}
+                id="selectedOption"
+                label="Options"
+                helper=""
+              >
+                <MenuItem value="">None</MenuItem>
+                <MenuItem value="frag">frag</MenuItem>
+                <MenuItem value="ipMenuItems spec">ipMenuItems spec</MenuItem>
+                <MenuItem value="tcpMenuItems spec">tcpMenuItems spec</MenuItem>
+                <MenuItem value="established">established</MenuItem>
+                <MenuItem value="setup">setup</MenuItem>
+                <MenuItem value="tcpflags spec">tcpflags spec</MenuItem>
+                <MenuItem value="icmptypes types">icmptypes types</MenuItem>
+              </SelectField> 
+            </Grid>
+          </Grid>
+         </Box>
        </Box>
      </Modal>
 
