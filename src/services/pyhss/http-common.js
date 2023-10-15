fc91074f8f76bf99fcc3224b6353334a1a9f73ca
Rudy (zarya) <git@gigafreak.net>
2023-10-15 08:57:55 +0200

    Fixed sending of Provisioning-Key

--- src/services/pyhss/http-common.js
+++ src/services/pyhss/http-common.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: localStorage.getItem("api"),
   headers: {
-    "Content-type": "application/json"
+    "Content-type": "application/json",
+    "Provisioning-Key": localStorage.getItem('token')
   }
 });
